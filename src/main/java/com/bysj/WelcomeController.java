3eab4662cf50aa8d5693cf21aaa4c6ccc67019ae
cz1996 <969176743@qq.com>
2018-04-09 12:44:53 +0800

    导入Vue前端框架

---
+++ src/main/java/com/bysj/WelcomeController.java
@@ -0,0 +1,27 @@
+package com.bysj;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by cz1996 on 2018/4/8.
+ */
+
+@Controller
+public class WelcomeController {
+    @RequestMapping("/")
+    public ModelAndView welcome(){
+        ModelAndView welcomeView = new ModelAndView();
+        welcomeView.setViewName("welcome");
+        return welcomeView;
+    }
+
+    @RequestMapping("/vueTest")
+    public  ModelAndView vueTest(){
+        ModelAndView vueView = new ModelAndView();
+        vueView.setViewName("vueTest");
+        return vueView;
+    }
+}
